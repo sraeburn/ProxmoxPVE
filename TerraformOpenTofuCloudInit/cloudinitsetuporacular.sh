9cd3f5c25203ee5cffcd12db372ebf5f774f866b
Steve Raeburn <steve@ninsky.com>
2025-08-10 11:37:17 -0700

    Update ubuntu version

--- TerraformOpenTofuCloudInit/cloudinitsetuporacular.sh
+++ TerraformOpenTofuCloudInit/cloudinitsetuporacular.sh
@@ -2,13 +2,13 @@
 # run these commands on the proxmox host as root
 
 # download the image
-wget https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
 
 # download libguestfs for the virt-customise tool 
 apt update -y && apt install libguestfs-tools -y
 
 # configure the image with qemu-guest-agent using virt customise
-virt-customize -a oracular-server-cloudimg-amd64.img --install qemu-guest-agent
+virt-customize -a noble-server-cloudimg-amd64.img --install qemu-guest-agent
 
 # create a new VM with VirtIO SCSI controller
 qm create 9010 --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
